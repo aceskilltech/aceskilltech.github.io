eaa4c113e86c9effc4735ba20283d86b31bf3d11
aceskilltech <141009509+aceskilltech@users.noreply.github.com>
2023-08-01 09:33:28 +0800

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,77 @@
+
+function showOverlay1() {
+    document.getElementById("cardOverlay1").style.display ="block";
+    document.getElementById("cardText1").style.display ="block";
+    document.getElementById("dCard1").style.transform ="scale(1.05)";
+} 
+
+function closeOverlay1() {
+    document.getElementById("cardOverlay1").style.display ="none";
+    document.getElementById("cardText1").style.display ="none";
+    document.getElementById("dCard1").style.transform ="scale(1)";
+} 
+
+
+function showOverlay2() {
+    document.getElementById("cardOverlay2").style.display ="block";
+    document.getElementById("cardText2").style.display ="block";
+    document.getElementById("dCard2").style.transform ="scale(1.05)";
+} 
+
+function closeOverlay2() {
+    document.getElementById("cardOverlay2").style.display ="none";
+    document.getElementById("cardText2").style.display ="none";
+    document.getElementById("dCard2").style.transform ="scale(1)";
+} 
+
+
+function showOverlay3() {
+    document.getElementById("cardOverlay3").style.display ="block";
+    document.getElementById("cardText3").style.display ="block";
+    document.getElementById("dCard3").style.transform ="scale(1.05)";
+} 
+
+function closeOverlay3() {
+    document.getElementById("cardOverlay3").style.display ="none";
+    document.getElementById("cardText3").style.display ="none";
+    document.getElementById("dCard3").style.transform ="scale(1)";
+} 
+
+
+function showOverlay4() {
+    document.getElementById("cardOverlay4").style.display ="block";
+    document.getElementById("cardText4").style.display ="block";
+    document.getElementById("dCard4").style.transform ="scale(1.05)";
+} 
+
+function closeOverlay4() {
+    document.getElementById("cardOverlay4").style.display ="none";
+    document.getElementById("cardText4").style.display ="none";
+    document.getElementById("dCard4").style.transform ="scale(1)";
+} 
+
+
+function showOverlay5() {
+    document.getElementById("cardOverlay5").style.display ="block";
+    document.getElementById("cardText5").style.display ="block";
+    document.getElementById("dCard5").style.transform ="scale(1.05)";
+} 
+
+function closeOverlay5() {
+    document.getElementById("cardOverlay5").style.display ="none";
+    document.getElementById("cardText5").style.display ="none";
+    document.getElementById("dCard5").style.transform ="scale(1)";
+} 
+
+
+function showOverlay6() {
+    document.getElementById("cardOverlay6").style.display ="block";
+    document.getElementById("cardText6").style.display ="block";
+    document.getElementById("dCard6").style.transform ="scale(1.05)";
+} 
+
+function closeOverlay6() {
+    document.getElementById("cardOverlay6").style.display ="none";
+    document.getElementById("cardText6").style.display ="none";
+    document.getElementById("dCard6").style.transform ="scale(1)";
+} 
